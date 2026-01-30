32c1407ceb686a377badc2634983f3c1abaac206
tqa <truongquocanh0706@gmail.com>
2026-01-30 11:17:20 +0700

    feature(auth): add auth middleware

--- fe/nuxt.config.ts
+++ fe/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['./app/auth'],  
+  extends: ['./app/auth', './app/home'],  
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
@@ -13,7 +13,11 @@ export default defineNuxtConfig({
   components: {
     dirs: [
       '~/components',
-      '~/app/auth/components'
+      '~/app/auth/components',
+      '~/app/home/components'
     ]
+  },
+  devServer: {
+    port: 5173
   }
 })
