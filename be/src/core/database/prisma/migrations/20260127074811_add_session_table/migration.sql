daa2ba65a0d09c68dc3e68b6c2a25aae334fe8d0
tqa <truongquocanh0706@gmail.com>
2026-02-02 13:57:30 +0700

    feature(database): update db schema, add seeding

--- be/src/core/database/prisma/migrations/20260127074811_add_session_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "sessions" (
-    "sid" TEXT NOT NULL,
-    "sess" JSONB NOT NULL,
-    "expire" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "sessions_pkey" PRIMARY KEY ("sid")
-);
